0c80a0fecf3f25f785e4eda1fa2fbe21f6559361
Georgi <georgy.keranov@gmail.com>
2021-08-28 01:50:25 +0300

    Add webpack in order to bundle js files in the theme

--- functions.php
+++ functions.php
@@ -156,7 +156,7 @@ function starter_theme_scripts() {
 	wp_enqueue_style( 'starter-theme-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'starter-theme-style', 'rtl', 'replace' );
 
-	// wp_enqueue_script( 'custom-script', get_template_directory_uri() . '/js/custom-script.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'starter-theme-script', get_template_directory_uri() . '/scripts.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
