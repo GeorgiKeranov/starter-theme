3ca6e965efd0ef07f22d5af8af277b6e3b0f3259
Georgi <georgy.keranov@gmail.com>
2021-08-28 18:54:01 +0300

    Add base footer html, styles and integration

--- custom-options/theme-options.php
+++ custom-options/theme-options.php
@@ -1,8 +1,19 @@
 <?php
-// use Carbon_Fields\Container\Container;
-// use Carbon_Fields\Field\Field;
+use Carbon_Fields\Container\Container;
+use Carbon_Fields\Field\Field;
 
-// Container::make( 'theme_options', __( 'Theme Options', 'starter-theme' ) )
-// 	->add_fields( array(
-// 		Field::make( 'text', 'starter_theme_text', __( 'Text Field', 'starter-theme' ),
-// 	) );
+Container::make( 'theme_options', __( 'Theme Options', 'starter-theme' ) )
+	->set_page_file( 'starter-theme-options.php' )
+	->add_tab( __( 'Socials', 'starter-theme' ), array(
+		Field::make( 'complex', 'starter_theme_socials', __( 'Socials', 'starter-theme' ) )
+			->set_layout( 'tabbed-horizontal' )
+			->add_fields( array(
+				Field::make( 'image', 'logo', __( 'Logo', 'crb' ) )
+					->set_help_text( __( 'Recommended logo size is 64x64 px', 'starter-theme' ) )
+					->set_width( 50 ),
+				Field::make( 'text', 'link', __( 'Link', 'crb' ) ),
+			) ),
+	) )
+	->add_tab( __( 'Footer', 'starter-theme' ), array(
+		Field::make( 'text', 'starter_theme_copyright', __( 'Copyright', 'starter-theme' ) )
+	) );
--- footer.php
+++ footer.php
@@ -13,8 +13,37 @@
 	</main><!-- /.main -->
 
 	<footer class="footer">
-		<h2>Footer</h2>
-	</footer><!-- /.footer -->
+		<div class="container">
+			<div class="footer__main">
+				<div class="footer__logo">
+					<?php starter_theme_the_logo(); ?>
+				</div><!-- /.footer__logo -->
+
+				<div class="footer__menu">
+					<?php if ( has_nav_menu( 'menu' ) ) {
+						wp_nav_menu( array(
+							'theme_location' => 'menu',
+							'container' => 'nav',
+							'container_class' => 'nav-footer'
+						) );
+					} ?>
+				</div><!-- /.footer__menu -->
+
+				<div class="footer__socials">
+					<?php get_template_part( 'template-parts/socials' ) ?>
+				</div><!-- /.footer__socials -->
+			</div><!-- /.footer__main -->
+
+			<?php
+			$copyright = carbon_get_theme_option( 'starter_theme_copyright' );
+
+			if ( !empty( $copyright ) ) : ?>
+				<div class="footer__bottom">
+					<p class="copyright"><?php echo do_shortcode( esc_html( $copyright ) ) ?></p>
+				</div><!-- /.footer__bottom -->
+			<?php endif; ?>
+		</div><!-- /.container -->
+	</footer>
 </div><!-- /.wrapper -->
 
 <?php wp_footer(); ?>
