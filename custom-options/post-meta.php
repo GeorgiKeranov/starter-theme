b3d3eb2239b61da87f9ac3a89ccc6be34677ee73
Georgi <georgy.keranov@gmail.com>
2021-08-29 17:28:51 +0300

    Add page builder functionality

--- custom-options/post-meta.php
+++ custom-options/post-meta.php
@@ -1,9 +1,20 @@
 <?php
-// use Carbon_Fields\Container;
-// use Carbon_Fields\Field;
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
 
-// Container::make( 'post_meta', __( 'Custom Data', 'starter-theme' ) )
-// 	->where( 'post_template', '=', 'templates/page-builder.php' )
-// 	->add_fields( array(
-// 		Field::make( 'text', 'starter_theme_text', __( 'Text Field', 'starter-theme' ) ),
-// 	) );
+/**
+ * Page Builder
+ */
+Container::make( 'post_meta', __( 'Page Builder', 'starter-theme' ) )
+	->where( 'post_template', '=', 'templates/page-builder.php' )
+	->add_fields( array(
+		Field::make( 'complex', 'starter_theme_page_builder_sections', 'Sections' )
+			->set_layout( 'tabbed-vertical' )
+
+			/**
+			 * Example section
+			 */
+			->add_fields( 'section-example', __( 'Section Example', 'starter-theme' ), array(
+				Field::make( 'text', 'title', __( 'Title', 'starter-theme' ) ),
+			) )
+	) );
---
+++ template-parts/page-builder/section-example.php
@@ -0,0 +1,5 @@
+<div class="section-example">
+	<?php if ( !empty( $args['title'] ) ) : ?>
+		<h2><?php echo esc_html( $args['title'] ) ?></h2>
+	<?php endif; ?>
+</div><!-- /.section-example -->
